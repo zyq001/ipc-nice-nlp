0bcdc4c55dab5d37c6ca458e1be2456e7ca3e196
zyq001 <mikezang625@gmail.com>
2017-02-24 13:10:16 +0800

    kick off

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "spark-neo4j"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/org/zyq/common/neo4j/NeoHelper.scala
@@ -0,0 +1,8 @@
+package org.zyq.common.neo4j
+
+/**
+  * Created by zyq on 2017/2/24.
+  */
+class NeoHelper {
+
+}
